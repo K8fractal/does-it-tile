ef7d1e43f43bb2272d313c4690bd87dd61a8e355
Kate Fractal <k8fractal@gmail.com>
2022-08-25 16:08:55 -0400

    part of last commit...

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,26 @@ const app = express();
 
 
 app.get('/hello/:name', (req,res)=> {
-    res.send('Hello '+req.params.name);
-    console.log(req);
-    //const id = req.query.id;
-    //res.status(200).json({ ID: id });
+    res.send('Hello '+req.params.name + ". Nice to meet you!");
 })
 
-app.get('/number', (req,res)=> {
-    res.send('27');
+app.get('/tile', (req,res)=> {
+    res.send('To get information about a tiling, give a description of regular polygons around a vertext. Try /tile/3.6');
+})
+
+app.get('/tile/:sides.:count', (req,res)=>{
+    const sides = Number(req.params.sides);
+    const count = Number(req.params.count)
+    if(isNaN(sides) || isNaN(count)){
+        res.status(400);
+        res.send('Tile requests must be in the form integer.integer')
+    }
+    else{
+        res.status(200);
+        res.send('STUB: Tiling description JSON here. Info about a tiling of '+count+ ' regular '+sides+'-gons: Coming soon.');
+//        res.send(regularTiling(sides,count))
+    }
+
 })
 
 const port = process.env.PORT || 3000;
