22c88158e372e99cd8d5f0017d783600ff8ee971
Kate Fractal <k8fractal@gmail.com>
2022-08-30 15:13:40 -0400

    Brian's test casses

--- src/tilings.test.ts
+++ src/tilings.test.ts
@@ -1,7 +1,20 @@
-import { interiorAngle} from "./tilings";
+import { interiorAngle, polygonName} from "./tilings";
 
 describe('testing interior angle formula', () => {
     test('interior angle of a triangle', () => {
         expect(interiorAngle(3)).toBe(60);
+    });
+    test('interior angle error cases', () => {
+        expect(() => interiorAngle(0)).toThrow();
+    })
+});
+
+describe('testing polygonName', () => {
+    test('names a square correctly', () => {
+        expect(polygonName(4)).toBe('square');
+    });
+    test('names n-gon correctly', () => {
+        expect(polygonName(27)).toBe('27-gon');
+        expect(polygonName(1)).toBe('1-gon');
     });
 });
