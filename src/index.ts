189b2a15722c6472c9d7e142729acce4e6ff89fe
Kate Fractal <k8fractal@gmail.com>
2022-09-09 14:03:12 -0400

    prettify index.js

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,45 @@
-import express from "express"
+import express from "express";
 import { Query } from "express-serve-static-core";
 import { regularTiling } from "./tilings";
 
-
 enum StatusCodes {
-    OK = 200,
-    BadRequest = 400,
-  }
+  OK = 200,
+  BadRequest = 400,
+}
 
 const app = express();
 
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello " + req.params.name + ". Nice to meet you!");
+});
 
-app.get('/hello/:name', (req,res)=> {
-    res.send('Hello '+req.params.name + ". Nice to meet you!");
-})
+app.get("/tile", (req, res) => {
+  res.send(
+    "To get information about a tiling, give a description of regular polygons around a vertext. Try /tile/3.6"
+  );
+});
 
-app.get('/tile', (req,res)=> {
-    res.send('To get information about a tiling, give a description of regular polygons around a vertext. Try /tile/3.6');
-})
-
-app.get('/tile/:sides.:count', (req,res)=>{
-    const sides = Number(req.params.sides);
-    const count = Number(req.params.count)
-    if(isNaN(sides) || isNaN(count)){
-        res.status(StatusCodes.BadRequest);
-        res.send('Tile requests can only contain numbers');
-    } else if(sides % 1 != 0 || count % 1 != 0){
-        res.status(StatusCodes.BadRequest);
-        res.send('Tile requests must be in the form integer.integer');
-    } else if(sides < 3){
-        res.status(StatusCodes.BadRequest);
-        res.send('Number of sides must be at least 3');
-    } else if(count < 3){
-        res.status(StatusCodes.BadRequest);
-        res.send('Number of tiles at a vertex must be at least 3');
-    } else{
-        res.status(StatusCodes.OK);
-    //    res.send('STUB: Tiling description JSON here. Info about a tiling of '+count+ ' regular '+sides+'-gons: Coming soon.');
-       res.send(regularTiling(sides,count))
-    }
-
-})
+app.get("/tile/:sides.:count", (req, res) => {
+  const sides = Number(req.params.sides);
+  const count = Number(req.params.count);
+  if (isNaN(sides) || isNaN(count)) {
+    res.status(StatusCodes.BadRequest);
+    res.send("Tile requests can only contain numbers");
+  } else if (sides % 1 != 0 || count % 1 != 0) {
+    res.status(StatusCodes.BadRequest);
+    res.send("Tile requests must be in the form integer.integer");
+  } else if (sides < 3) {
+    res.status(StatusCodes.BadRequest);
+    res.send("Number of sides must be at least 3");
+  } else if (count < 3) {
+    res.status(StatusCodes.BadRequest);
+    res.send("Number of tiles at a vertex must be at least 3");
+  } else {
+    res.status(StatusCodes.OK);
+    res.send(regularTiling(sides, count));
+  }
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`App listening on PORT ${port}`))
+app.listen(port, () => console.log(`App listening on PORT ${port}`));
