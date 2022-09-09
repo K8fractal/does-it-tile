b5e77355db059bc4dcab63bec0271355ea56d3df
Kate Fractal <k8fractal@gmail.com>
2022-09-09 09:44:38 -0400

    polyhedra names & prettier

--- src/tilings.test.ts
+++ src/tilings.test.ts
@@ -1,39 +1,47 @@
 import { register } from "ts-node";
-import { interiorAngle, polygonName, regularTiling, tilingType} from "./tilings";
+import {
+  interiorAngle,
+  polygonName,
+  regularTiling,
+  tilingType,
+} from "./tilings";
 
-describe('testing interior angle formula', () => {
-    test('interior angle of a triangle', () => {
-        expect(interiorAngle(3)).toBe(60);
-    });
-    test('interior angle of a regular pentagon', () => {
-        expect(interiorAngle(5)).toBe(108);
-    });
-    test('interior angle error cases', () => {
-        expect(() => interiorAngle(0)).toThrow();
-    })
+describe("testing interior angle formula", () => {
+  test("interior angle of a triangle", () => {
+    expect(interiorAngle(3)).toBe(60);
+  });
+  test("interior angle of a regular pentagon", () => {
+    expect(interiorAngle(5)).toBe(108);
+  });
+  test("interior angle error cases", () => {
+    expect(() => interiorAngle(0)).toThrow();
+  });
 });
 
-describe('testing polygonName', () => {
-    test('names a square correctly', () => {
-        expect(polygonName(4)).toBe('square');
-    });
-    test('names n-gon correctly', () => {
-        expect(polygonName(27)).toBe('27-gon');
-        expect(polygonName(30)).toBe('30-gon');
-    });
-    test('minimum number of sides',()=> {
-        expect(() => polygonName(1)).toThrow();
-    });
+describe("testing polygonName", () => {
+  test("names a square correctly", () => {
+    expect(polygonName(4)).toBe("square");
+  });
+  test("names n-gon correctly", () => {
+    expect(polygonName(27)).toBe("27-gon");
+    expect(polygonName(30)).toBe("30-gon");
+  });
+  test("minimum number of sides", () => {
+    expect(() => polygonName(1)).toThrow();
+  });
 });
 
-describe('testing regularTiling',() => {
-    test('tesselations',() => {
-        expect(regularTiling(3,6).polygon).toBe('triangle');
-        expect(regularTiling(3,6).curvatureType).toBe('TESSELLATION');
-        expect(regularTiling(4,4).curvatureType).toBe('TESSELLATION');
-        expect(regularTiling(6,3).curvatureType).toBe('TESSELLATION');
-    });
-    test('polyhedra', ()=>{
-        expect(regularTiling(3,4).curvatureType).toBe('POLYHEDRON');
-    });
+describe("testing regularTiling", () => {
+  test("tesselations", () => {
+    expect(regularTiling(3, 6).polygon).toBe("triangle");
+    expect(regularTiling(3, 6).curvatureType).toBe("TESSELLATION");
+    expect(regularTiling(4, 4).curvatureType).toBe("TESSELLATION");
+    expect(regularTiling(6, 3).curvatureType).toBe("TESSELLATION");
+    expect(regularTiling(3, 6).name).toBe(undefined);
+  });
+  test("polyhedra", () => {
+    expect(regularTiling(3, 4).curvatureType).toBe("POLYHEDRON");
+    expect(regularTiling(4, 3).name).toBe("Cube");
+    expect(regularTiling(3, 3).name).toBe("Tetrahedron");
+  });
 });
--- src/tilings.ts
+++ src/tilings.ts
@@ -1,65 +1,99 @@
-export type tilingType = "TESSELLATION" | "POLYHEDRON" | "HYPERBOLIC" | "BRANCHING";
+export type tilingType =
+  | "TESSELLATION"
+  | "POLYHEDRON"
+  | "HYPERBOLIC"
+  | "BRANCHING";
 interface RegularTiling {
-    polygon: string;
-    count: number;
-    curvatureType: tilingType;
-    name?: string;
+  polygon: string;
+  count: number;
+  curvatureType: tilingType;
+  name?: string;
 }
+
+// const regularPolyhedraNames = new Map<[number, number], string>([
+//   [[3, 3], "Tetrahedron"],
+//   [[3, 4], "Octahedron"],
+//   [[3, 5], "Icosahedron"],
+//   [[4, 3], "Cube"],
+//   [[5, 3], "Dodecahedron"],
+// ]);
+
+// inelegant hack: index is 1000*sides+count
+const regularPolyhedraNames = new Map<number, string>([
+  [3003, "Tetrahedron"],
+  [3004, "Octahedron"],
+  [3005, "Icosahedron"],
+  [4003, "Cube"],
+  [5003, "Dodecahedron"],
+]);
+console.log(regularPolyhedraNames.get(3003));
 
 //regularTiling(sides,count)
 
 export const regularTiling = (sides: number, count: number): RegularTiling => {
-  let angleSum = interiorAngle(sides)*count;
+  let angleSum = interiorAngle(sides) * count;
   // first check
-  let regularTilingType:tilingType;
-  if (angleSum < 360){
-    regularTilingType = 'POLYHEDRON';
+  let regularTilingType: tilingType;
+  let name: string | undefined;
+  if (angleSum < 360) {
+    regularTilingType = "POLYHEDRON";
     //additional checks: not needed for regular case
-  } else if (angleSum == 360){
+    name = regularPolyhedraNames.get(sides * 1000 + count);
+  } else if (angleSum == 360) {
     //additional checks: not needed for regular case
-    regularTilingType = 'TESSELLATION';
-  } else { //angleSum > 360
+    regularTilingType = "TESSELLATION";
+  } else {
+    //angleSum > 360
     regularTilingType = "HYPERBOLIC";
   }
-  
-  return({
-        polygon: polygonName(sides),
-        count: count,
-        curvatureType: regularTilingType,
-        name: "Cube",
-      });
+
+  return {
+    polygon: polygonName(sides),
+    count: count,
+    curvatureType: regularTilingType,
+    name,
   };
+};
 
-  const cube: RegularTiling = {
-    polygon: "square",
-    count: 3,
-    curvatureType: "POLYHEDRON",
-    name: "Cube",
-  }
+const cube: RegularTiling = {
+  polygon: "square",
+  count: 3,
+  curvatureType: "POLYHEDRON",
+  name: "Cube",
+};
 
-  export const polygonName = (sides: number): string => {
-    switch(sides){
-        case 3: return("triangle");
-        case 4: return("square");
-        case 5: return("pentagon");
-        case 6: return("hexagon");
-        case 7: return("heptagon");
-        case 8: return("octagon");
-        case 9: return("nonagon");
-        case 10: return("decagon");
-        case 12: return("dodecagon");
-        default: 
-          if(sides < 3){
-            throw new Error("polygons must have at least 3 sides");
-          }
-          return(sides+"-gon");
-    }
+export const polygonName = (sides: number): string => {
+  switch (sides) {
+    case 3:
+      return "triangle";
+    case 4:
+      return "square";
+    case 5:
+      return "pentagon";
+    case 6:
+      return "hexagon";
+    case 7:
+      return "heptagon";
+    case 8:
+      return "octagon";
+    case 9:
+      return "nonagon";
+    case 10:
+      return "decagon";
+    case 12:
+      return "dodecagon";
+    default:
+      if (sides < 3) {
+        throw new Error("polygons must have at least 3 sides");
+      }
+      return sides + "-gon";
   }
+};
 
-  //working in degrees
-  export const interiorAngle = (sides: number): number => {
-    if(sides < 3){
-      throw new Error("polygons must have at least 3 sides");
-    }
-    return(180-360/sides);
+//working in degrees
+export const interiorAngle = (sides: number): number => {
+  if (sides < 3) {
+    throw new Error("polygons must have at least 3 sides");
   }
+  return 180 - 360 / sides;
+};
